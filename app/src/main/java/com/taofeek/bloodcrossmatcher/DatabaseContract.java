db7f8d32fb8f5d650f16debc0d5094b7176897cb
EngrTaofeek <oduola.taofeekkola@gmail.com>
2020-07-10 14:49:54 +0100

    Creating database contract

---
+++ app/src/main/java/com/taofeek/bloodcrossmatcher/DatabaseContract.java
@@ -0,0 +1,20 @@
+package com.taofeek.bloodcrossmatcher;
+
+import android.provider.BaseColumns;
+
+public class DatabaseContract {
+    private DatabaseContract(){}
+    public static final class Authentication implements BaseColumns {
+        public static final String TABLE_NAME = "authentication";
+        public static final String COLUMN_EMAIL = "email";
+        public static final String COLUMN_PASSWORD = "password";
+
+        //CREATE TABLE course_info (course_id , course title);
+        public static final String SQL_CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + " (" +
+                _ID + " INTEGER PRIMARY KEY, " +
+                COLUMN_EMAIL + " TEXT UNIQUE NOT NULL, " + "," +
+                COLUMN_PASSWORD + " TEXT NOT NULL " + ")" ;
+
+
+    }
+}
