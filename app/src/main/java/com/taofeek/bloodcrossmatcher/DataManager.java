24b0f3fa7eb9c717f940be88ed46452396e76ffd
EngrTaofeek <oduola.taofeekkola@gmail.com>
2020-07-10 20:20:41 +0100

    managing authentication table

---
+++ app/src/main/java/com/taofeek/bloodcrossmatcher/DataManager.java
@@ -0,0 +1,19 @@
+package com.taofeek.bloodcrossmatcher;
+
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+
+import static java.security.AccessController.getContext;
+
+public class DataManager {
+   OpenHelper mdb;
+    public void insertDataRegister(String email, String password){
+       // OpenHelper dbHelper = new OpenHelper();
+        SQLiteDatabase db = mdb.getReadableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(DatabaseContract.Authentication.COLUMN_EMAIL, email);
+        values.put(DatabaseContract.Authentication.COLUMN_PASSWORD,password);
+        long newRowId = db.insert(DatabaseContract.Authentication.TABLE_NAME, null, values);
+
+    }
+}
--- app/src/main/java/com/taofeek/bloodcrossmatcher/OpenHelper.java
+++ app/src/main/java/com/taofeek/bloodcrossmatcher/OpenHelper.java
@@ -6,13 +6,22 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import java.security.AccessControlContext;
+
 public class OpenHelper extends SQLiteOpenHelper {
-    public OpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public static final String DATABASE_NAME = "BloodMatcher.db";
+    public static final int DATABASE_VERSION = 1;
+    private final Context context;
+    public OpenHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+
+        this.context = null;
     }
+
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        db.execSQL(DatabaseContract.Authentication.SQL_CREATE_TABLE);
 
     }
 
