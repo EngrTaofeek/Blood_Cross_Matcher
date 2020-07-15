9d78d8e3b8428a69fa53e9db99068ffbf3c6a24d
EngrTaofeek <oduola.taofeekkola@gmail.com>
2020-07-15 22:59:55 +0100

    populating spinner

---
+++ app/src/main/java/com/taofeek/bloodcrossmatcher/EditProfile.java
@@ -0,0 +1,29 @@
+package com.taofeek.bloodcrossmatcher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class EditProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_profile);
+        populateSpinner();
+
+    }
+
+    private void populateSpinner() {
+        Spinner spinner = (Spinner) findViewById(R.id.edit_profile_spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+    }
+}
