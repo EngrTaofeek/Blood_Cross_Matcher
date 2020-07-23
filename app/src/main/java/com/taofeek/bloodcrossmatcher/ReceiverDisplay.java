2dc1c6421455583742b77100e4f3781a693853a1
EngrTaofeek <oduola.taofeekkola@gmail.com>
2020-07-23 02:00:39 +0100

    receiver's donor recyclerview layout design

---
+++ app/src/main/java/com/taofeek/bloodcrossmatcher/ReceiverDisplay.java
@@ -0,0 +1,14 @@
+package com.taofeek.bloodcrossmatcher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReceiverDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receiver_display);
+    }
+}
